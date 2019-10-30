7cdd2a247a79f403f67d1957b56aae5447d18bb3
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-30 20:30:52 +0100

    remove comments from redux

--- web-store/client/src/redux/shopRedux.js
+++ web-store/client/src/redux/shopRedux.js
@@ -1,23 +1,8 @@
-const initialState = {
-
-  };
-
-//// Actions
-// action name creator
-// const reducerName = 'shop';
-// const createActionName = name => `app/${reducerName}/${name}`;
-
-// action exports
-// export const ACTION_NAME = createActionName('ACTION_NAME');
-
-// export const actionName = payload => ({ payload, type: ACTION_NAME });
-
+const initialState = {};
 
 //// Reducer
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
- //   case ACTION_NAME:
- //     return { ...statePart, /*data: action.payload*/ };
     default:
       return statePart;
   }
