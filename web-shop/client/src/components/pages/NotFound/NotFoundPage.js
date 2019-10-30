6e0257ab6f4c233479d3d4cb750e729dd8cf0a39
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-30 22:55:28 +0100

    add 404 page

--- web-shop/client/src/components/pages/NotFound/NotFoundPage.js
+++ web-shop/client/src/components/pages/NotFound/NotFoundPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const NotFoundPage = () => (
   <div>
-    <h1>Home Page</h1>
+    <h1>404 Page Not Found</h1>
+    <p>Something went wrong or link no longer exists</p>
   </div>
 );
 
