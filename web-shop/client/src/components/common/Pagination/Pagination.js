37c58f1e3401059669270983bf743a0a82c7c94e
Bartek Bugała <bartek.bugala@gmail.com>
2019-11-03 12:41:01 +0100

    basic pagination template

---
+++ web-shop/client/src/components/common/Pagination/Pagination.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+
+import './Pagination.scss';
+
+class Pagination extends React.Component {
+  state = { presentPage: this.props.presentPage || (this.props.initialPage || 1) };
+
+  changePage = page => {
+    // for lifted up state
+    if (this.props.presentPage) {
+      this.props.onPageChange(page);
+    }
+    // for local state
+    else {
+      const { onPageChange } = this.props;
+      this.setState({ presentPage: page });
+      onPageChange(page);
+    }
+  };
+
+  render() {
+    const { pages } = this.props;
+    const { presentPage } = this.state;
+    const { changePage } = this;
+
+    return (
+      <div className="pagination">
+        <ul className="pagination__list">
+          <li
+            className={`pagination__list__item${presentPage === 1 ? ' pagination__list__item--hidden' : ''}`}
+            onClick={() => changePage(presentPage - 1)}
+          >
+            <MdKeyboardArrowLeft />
+          </li>
+
+          {[...Array(pages)].map((el, page) => (
+            <li
+              key={++page}
+              onClick={() => changePage(page)}
+              className={`pagination__list__item${page === presentPage ? ' pagination__list__item--active' : ''}`}
+            >
+              {page}
+            </li>
+          ))}
+
+          <li
+            className={`pagination__list__item${presentPage === pages ? ' pagination__list__item--hidden' : ''}`}
+            onClick={() => changePage(presentPage + 1)}
+          >
+            <MdKeyboardArrowRight />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+Pagination.propTypes = {
+  pages: PropTypes.number.isRequired,
+  initialPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+};
+
+export default Pagination;
