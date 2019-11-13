5d9783e74b7b2a6f91d0f4d6d80fb535a29d0f0a
Bartek.B <bartek.bugala@gmail.com>
2019-11-13 20:55:22 +0100

    modified:   server/controllers/discount.controller.js

--- web-shop/server/controllers/discount.controller.js
+++ web-shop/server/controllers/discount.controller.js
@@ -4,7 +4,12 @@ const Discount = require('../models/discount.model');
 // get single discount
 exports.getSingleDiscount = async (req, res) => {
   try {
-    res.status(200).json(await Discount.findOne({ code: req.params.code }));
+    let responseDiscount = await Discount.findOne({ code: req.params.code });
+    if (responseDiscount) {
+      res.status(200).json(responseDiscount);
+    } else {
+      res.status(200).json('null');
+    }
   } catch (err) {
     res.status(500).res.json(err);
   }
