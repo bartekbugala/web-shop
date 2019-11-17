1174932a9c566182eca626d0602ad939b9634d98
Bartek.B <bartek.bugala@gmail.com>
2019-11-17 01:34:59 +0100

    build

---
+++ web-shop/client/build/precache-manifest.13c82fb34476fe84dd1c59bd93201053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5827484ec59613df01a6b99dce3418b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f4558da6eed73f336a5",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9197e2512ff5fecdccde",
+    "url": "/static/css/main.72230e68.chunk.css"
+  },
+  {
+    "revision": "0f4558da6eed73f336a5",
+    "url": "/static/js/2.da1fcc5b.chunk.js"
+  },
+  {
+    "revision": "9197e2512ff5fecdccde",
+    "url": "/static/js/main.faed8e15.chunk.js"
+  },
+  {
+    "revision": "7894831175f9bc56b371",
+    "url": "/static/js/runtime-main.d64596fc.js"
+  }
+]);
