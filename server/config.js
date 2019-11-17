e8336624690ee7f2b120fe74d52af71bbbbe7696
Bartek.B <bartek.bugala@gmail.com>
2019-11-17 15:00:55 +0100

    add comment to config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: 8000,
+  // Uncomment bottom line for local DB
   //MONGO_URL: 'mongodb://localhost:27017/webshop'
   MONGO_URL: `mongodb+srv://bb-dev:!M0ng0db2019@cluster0-dbftg.mongodb.net/test?retryWrites=true&w=majority`
 };
